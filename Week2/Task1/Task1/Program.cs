9bc2244532aa1d63beab5111f75e22887b849bca
Aruzhan <aruzhs@gmail.com>
2019-02-16 05:18:33 +0600

    Task1

--- Week2/Task1/Task1/Program.cs
+++ Week2/Task1/Task1/Program.cs
@@ -10,16 +10,16 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            string text = File.ReadAllText(@"C:\Users\aruzh\source\repos\Week2\Task1\Task1\input.txt");
-            for (int i = 0, j = text.Length - 1; i < j; i++, j--)
+            string text = File.ReadAllText(@"C:\Users\aruzh\source\repos\Week2\Task1\Task1\input.txt");// считываем стринг через инпут файл
+            for (int i = 0, j = text.Length - 1; i < j; i++, j--)//цикл создан чтоб узнать является ли стринг палидромом
             {
-                if (text[i] != text[j])
+                if (text[i] != text[j])//если эти буквы будут не одинаковыми (то есть допустим первая и посл буква и тд)
                 {
-                    Console.WriteLine("No");
-                    return;
+                    Console.WriteLine("No");//выводим нет
+                    return;//завершаем выполнение метода
                 }
             }
-            Console.WriteLine("Yes");
+            Console.WriteLine("Yes");//если же те буквы равны тогда выводим да
         }
     }
 }
